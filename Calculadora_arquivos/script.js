feb47a74d1e4c168d2ad7e4d8f39d2fee75940c8
Saymon_Kauffmann <101084320+Saymon-Kauffmann@users.noreply.github.com>
2022-03-07 20:08:01 -0300

    Calculator Codes
    Commitando os codigos da calculadora

---
+++ Calculadora_arquivos/script.js
@@ -0,0 +1,38 @@
+function fazer_calculo() {
+    let primeiro_numero = document.getElementById('primeiroNumero')
+    let segundo_numero = document.getElementById('segundoNumero')
+    let mostraRESULTADO = document.getElementById('tabela')
+    let P_NUM = Number(primeiro_numero.value)
+    let S_NUM = Number(segundo_numero.value)
+
+    let operacoes = document.getElementsByName('operaçao')
+    if (operacoes[0].checked) {
+        let res_op = P_NUM + S_NUM
+        let item = document.createElement('option')
+        item.text = `${P_NUM} + ${S_NUM} = ${res_op}`
+        mostraRESULTADO.appendChild(item)
+    }
+
+    if (operacoes[1].checked) {
+        let res_op = P_NUM - S_NUM
+        let item = document.createElement('option')
+        item.text = `${P_NUM} - ${S_NUM} = ${res_op}`
+        mostraRESULTADO.appendChild(item)
+    }
+
+    if (operacoes[2].checked) {
+        let res_op = P_NUM / S_NUM
+        let item = document.createElement('option')
+        item.text = `${P_NUM} ÷ ${S_NUM} = ${res_op}`
+        mostraRESULTADO.appendChild(item)
+    } 
+
+    if (operacoes[3].checked) {
+        let res_op = P_NUM * S_NUM
+        let item = document.createElement('option')
+        item.text = `${P_NUM} x ${S_NUM} = ${res_op}`
+        mostraRESULTADO.appendChild(item)
+    }
+    
+}
+
